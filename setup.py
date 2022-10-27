c6efcc7c8d9895cb260936062421247ba5a8a6ca
Rodrigo Rodrigues Scotti <rrscotti@hotmail.com>
2022-10-26 23:04:13 -0300

    removendo configuração flask

--- setup.py
+++ setup.py
@@ -1,10 +1,8 @@
-# -*- coding: utf-8 -*-
-
 from setuptools import find_packages, setup
 
 __version__ = '0.1.0'
 __description__ = 'Template Python'
-__long_description__ = 'Project configuration template Python using microframework Flask'
+__long_description__ = 'Project configuration template Python using FastAPI'
 
 __author__ = 'Rodrigo Rodrigues'
 __author_email__ = 'rrscotti@hotmail.com'
@@ -19,7 +17,7 @@ setup(
 	description = __description__,
 	long_description = __long_description__,
 	url = 'https://github.com/RodriguesRodrigo/init-python',
-	keywords = 'Template, Configuration, Python, Flask',
+	keywords = 'Template, Configuration, Python, FastAPI',
 	include_package_data = True,
 	zip_safe = False,
 	classifiers = [
@@ -27,7 +25,7 @@ setup(
 		'Operating System :: OS Independent',
 		'Topic :: Software Development',
 		'Environment :: Web Environment',
-		'Programming Language :: Python :: 3.7',
+		'Programming Language :: Python :: 3.10',
 		'License :: OSI Approved :: MIT License',
 	],
 )
